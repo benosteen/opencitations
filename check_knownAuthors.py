37381caea4354602224f375080e6751cdafac43d
John Mansfield <jbmansfield@gmail.com>
2010-08-02 17:14:33 +0100

    committed

---
+++ check_knownAuthors.py
@@ -0,0 +1,47 @@
+import re
+from elementtree import ElementTree
+
+def check_knownAuthors (authorData, triples):
+
+	#Switch this module off;
+	return None
+
+	#No fuzziness implemented. Just checks all the fields to see if they all match up against some existing record
+	knownAuthor = None
+	
+	if len(authorData.keys()) == 0:
+		return None
+	
+	existingAuthors = {} # Clone the triples dictionary
+	for item in triples.keys():
+		if triples[item].has_key('authorMainName'):
+			existingAuthors[item] = triples[item]
+	
+	for metaField in authorData.keys():
+		value = authorData[metaField]
+		
+		#Go through the collection of known entities, throwing away anything that doesn't match this value
+		for entity in existingAuthors.keys():
+			# First check if the entity even has this field, and if not skip it
+			if not existingAuthors[entity].has_key(metaField):
+				continue
+			
+			if type(value).__name__=='dict':
+				for key in existingAuthors[entity][metaField].keys():
+					if not existingAuthors[entity][metaField][key] == value[key]:
+						#print "throwaway1"
+						del existingAuthors[entity] #throw it away
+			elif not existingAuthors[entity][metaField] == value:
+				#print "throwaway2"
+				del existingAuthors[entity] #throw it away
+	
+	#whatever is left must have all the same param values - i.e. it's a match
+	for entity in existingAuthors.keys():
+		knownAuthor = entity
+		print "For this author: "
+		print authorData
+		print "I found a matching author: "
+		for key in existingAuthors[knownAuthor].keys():
+			print str(key) + ": " + str(existingAuthors[knownAuthor][key])
+	
+	return knownAuthor
---
+++ convert-to-turtle.py
@@ -0,0 +1,127 @@
+"""
+convert-to-turtle.py 
+
+Read RDF data in XML or any triples format, does some cleanup, and outputs as Turtle RDF
+"""
+
+import sys, re, os
+from read_triples import read_triples
+from namespace_authorities import get_nsAuthorities
+from output_vocabulary import get_vocabulary
+from dir_navigator import Walk
+
+#Provide command-line arguments: input-file, input-format
+if not len(sys.argv)==3:
+	sys.exit("Program requires two command-line arguments: 1) top-level directory for source data; 2) source file format\n")
+
+sourceDir = sys.argv[1]
+inputFormat = sys.argv[2]
+
+print "Source directory: " + str(sourceDir) + "..."
+print "Input format: " + str(inputFormat) + "..."
+if not re.search('(xml|triple)', inputFormat, re.IGNORECASE):
+	sys.exit("Input format can't be identified\n")
+
+# Go through source directoy structure, making transformed version of every source file
+sourceFiles = [] 
+if re.search('xml', inputFormat, re.IGNORECASE):
+	sourceFiles = Walk(sourceDir, 1, '^.+\.n?xml$')
+elif re.search('triple', inputFormat, re.IGNORECASE):
+	sourceFiles = Walk(sourceDir, 1, "^.+.txt$")
+	
+outputDir = re.sub('/$', '', sourceDir)
+outputDir = outputDir + '_Turtle-RDF/'
+	
+for inputFileName in sorted(sourceFiles):
+	print "Doing file: " + str(inputFileName) + "..."
+
+	#Output filename is same as input, but with changed top-level directory, and changed file extension
+	outputFileName = re.sub(sourceDir, outputDir, inputFileName)
+	outputFileName = re.sub('\.(txt|xml|nxml)$', '', outputFileName)
+	outputFileName = outputFileName + ".n3"
+	
+	outputPath = re.sub('/[^/]+$', '/', outputFileName)
+	#print outputPath
+	if not os.path.isdir(outputPath):
+		os.makedirs(outputPath) #NB this function makes all intermediate dirs as required
+	
+	#Filehandles
+	inputFile = open(inputFileName, 'r')
+	outputFile = open(outputFileName, 'w')
+
+	#Print a header to the output file
+	outputFile.write ("""######
+	## Turtle RDF data, generated using convert-to-turtle.py 
+	## at the Open Citations Project, Image Bioinformatics Research Group,
+	## University of Oxford. See http://ibrg.zoo.ox.ac.uk
+	######
+
+	""")
+	outputFile.write ("## Source file: " + str(inputFileName) + "\n\n")
+
+	#Get namespace authorities from separate manifest
+	authorities = get_nsAuthorities()
+
+	#List all the authority prefixes at the top of the output file
+	for auth in sorted(authorities.keys()):
+		outputFile.write("@prefix\t" + authorities[auth] + "\t<" + auth + ">.\n")
+
+	outputFile.write("\n")
+
+
+	#Read the inputFile in to a triples dictionary. In fact it will be a multidimensional dictionary, taking the form triples[subject][predicate]
+	triples = read_triples(inputFile, inputFormat, authorities)
+
+
+	# Output rdf hash in standard Turtle format
+	for subject in sorted(triples.keys()):
+		outputFile.write(subject + "\n")
+		
+		#Add an rdf:type to all journal article and author graphs, if they don't have one
+		if not triples[subject].has_key('entityType') or triples[subject].has_key('rdf:type'):
+			if triples[subject].has_key('artTitle'):
+				triples[subject]['entityType'] = ['fabio:Article']
+			elif triples[subject].has_key('authorMainName'):
+				triples[subject]['entityType'] = ['foaf:Person']
+		
+		pos = 0 #position as we cycle through the list of keys
+		for predicate in sorted(triples[subject].keys()):
+			values = triples[subject][predicate]
+			
+			# # # Here we are going to make sure the output value is a string
+			if type(values).__name__=='dict':
+				valueList = []
+				for key in values.keys():
+					value = 0
+					if type(values[key]).__name__=='str':
+						value = values[key]
+					elif type(values[key]).__name__=='list':
+						value = str(", ".join(values[key]))
+					displayKey = str(get_vocabulary(key))
+					valueList.append(displayKey + ' "' + value + '"')
+				valueString = "[\n\t\t" + str(";\n\t\t".join(valueList)) + "\n\t]"
+				values = valueString
+			if type(values).__name__=='str':
+				values = values #It's already a string
+			elif type(values).__name__=='list':
+				values = str(", ".join(values))
+			else:
+				print "Couldn't estbalish data type for output value: " + str(type(values).__name__)
+			# # #
+				
+			displayPredicate = str(get_vocabulary(predicate))
+			outputFile.write("\t" + displayPredicate + "\t" + values)
+			
+			#Last one should be followed by full stop
+			if pos + 1 == len(triples[subject].keys()):
+				outputFile.write(".\n\n")
+			
+			#All others use a semi-colon
+			else:
+				outputFile.write(";\n")
+			
+			pos += 1
+		
+
+	inputFile.close()
+	outputFile.close()
